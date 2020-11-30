694f0d42a007e5c48c530e91ecdd4c76a44a38d1
Devon Adams <73118441+devonadamsofficial@users.noreply.github.com>
2020-11-30 00:20:28 -0600

    Added rMIDI.h
    The basic header file for reading MIDI data.
    Doesn't do anything

---
+++ Version0/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <vector>
+#include <array>
+#include <string>
+
+#include "rMIDI.h"
+
+#include <vulkan/vulkan.h>
+
+std::vector <std::string> dHistory;
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}	//	END of main()
